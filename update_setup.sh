3b029047d98d703ef4b263d5dffca2830630edab
proboticks <st.viveque@gmail.com>
2026-02-11 13:19:49 -0700

    fix ROS DOMAIN by removing preceeding 0s

--- update_setup.sh
+++ update_setup.sh
@@ -7,7 +7,11 @@ STATE_FILE="$HOME/.vm_setup_state"
 
 # Check if the source file exists
 if [ -f "$SOURCE_FILE" ]; then
-    # 1. Extract IDs and Server info
+
+    # 1. Fix Domain ID by precedding precedding(ex: 03 -> 3) to prevent issues with RViz
+    sudo sed -i 's/ROS_DOMAIN_ID=0\([0-9]\)/ROS_DOMAIN_ID=\1/g' $SOURCE_FILE
+
+    # 2. Extract IDs and Server info
     # We use 'source' in a subshell to get the variables without affecting this script's environment
     DOMAIN_ID=$(grep "ROS_DOMAIN_ID=" "$SOURCE_FILE" | sed 's/.*ROS_DOMAIN_ID=\([0-9]*\).*/\1/')
     DISCOVERY_SERVER=$(grep "ROS_DISCOVERY_SERVER=" "$SOURCE_FILE" | sed -e 's/.*export ROS_DISCOVERY_SERVER="//' -e 's/".*//')
@@ -15,16 +19,16 @@ if [ -f "$SOURCE_FILE" ]; then
     # Save the ID to the hidden file as before
     echo "$DOMAIN_ID" > "$DEST_FILE"
     
-    # 2. Get Git Version and Timestamp
+    # 3. Get Git Version and Timestamp
     GIT_VERSION=$(git rev-parse --short HEAD 2>/dev/null || echo "not_a_repo")
     TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
-    # 3. Copy new tmux file
+    # 4. Copy new tmux file
     cp ~/.tmux.conf ~/.tmux_$(date +%Y%m%d_%H%M%S) 
     rm ~/.tmux.conf
     ln -sf $HOME/vm_setup/tmux.conf ~/.tmux.conf
 
-    # 3. Append the comma-separated line to the state file
+    # 5. Append the comma-separated line to the state file
     # Format: hash, timestamp, bot_id, domain_id, discovery_server
     echo "$GIT_VERSION, $TIMESTAMP, $DOMAIN_ID, $DISCOVERY_SERVER" >> "$STATE_FILE"
 
